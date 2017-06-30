3c732ebcfd0c02564d7683ea983b52ade2430444
Patel Monil (YPB6YXF) <monilpatel@ups.com>
2017-06-29 22:16:08 -0400

    merge conflit

--- app.js
+++ app.js
@@ -12,21 +12,3 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('listening to %s', server.url);
 });
 // Send welcome when conversation with bot is started, by initiating the root dialog
-bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded) {
-        message.membersAdded.forEach(function (identity) {
-            if (identity.id === message.address.bot.id) {
-                bot.beginDialog(message.address, 'order_like_more');
-            }
-        });
-    }
-});
-bot.dialog('order_like_more',[
-    function(session){
-        builder.Prompts.text(session,"Would you like to order more ?");
-    },
-    function (session,results){
-        var res = results.response;
-        session.send(res);
-    }
-]);
